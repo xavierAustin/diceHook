ed62029bc180b0ea3092d1895b90b8abe38092b8
Xavier Austin <166980912+xavierAustin@users.noreply.github.com>
2025-05-25 00:51:51 -0700

    initial commit

---
+++ js/input.js
@@ -0,0 +1,11 @@
+class INPUTS{
+    constructor(){
+        this.buffer = {jmp:0,use:0,l:0,u:0,d:0,r:0,rjmp:0,ruse:0,rl:0,ru:0,rd:0,rr:0};
+        this.onKeyPress = new EventTarget();
+    }
+    update(){
+        if (1);
+            
+        //window.addEventListener("keydown", function(e) { if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {e.preventDefault();}}, false);
+    }
+}
---
+++ js/player.js
@@ -0,0 +1,8 @@
+class PLAYER{
+    constructor(){
+        this.dx = 0;
+        this.dy = 0;
+        this.y = 0;
+        this.x = 0;
+    }
+}
